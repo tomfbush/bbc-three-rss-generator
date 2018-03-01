bb33e61311def0fe1fd0412f55a3d53af4a12538
tomfbush <tomfbush@gmail.com>
2018-03-01 16:26:38 +0000

    changed to rss2producer to get round encoding issue. didn't work so just fixed it with re instead

--- rss-feed-three.py
+++ rss-feed-three.py
@@ -3,9 +3,11 @@
 #############
 
 import datetime
-import PyRSS2Gen
+# import PyRSS2Gen
+from rss2producer import RSS2Feed
 import requests
 from bs4 import BeautifulSoup
+import re
 
 ############
 # settings #
@@ -46,7 +48,6 @@ soup = BeautifulSoup(page, 'lxml')
 
 urls = []
 titles = []
-dictForRSS = {}
 
 ##########################
 # extract promo elements #
@@ -62,6 +63,7 @@ for promo in promo_list:
     # clean up h3 element to leave only heading
     for title in titlesRaw:
         title = title.text.strip()  # strip() is used to remove starting and trailing
+        title = re.sub(u"(\u2018|\u2019)", "'", title)
         titles.append(title)
 
     # for the bits we've found, append them to the two empty lists
@@ -74,21 +76,37 @@ for promo in promo_list:
 # See: channel required elements -- see https://cyber.harvard.edu/rss/rss.html for extended list
 # See: see https://pypi.python.org/pypi/PyRSS2Gen for building feed
 
-RSSItemList = []
-for Title, Link in zip(titles, urls):
-    RSSItemList.append(PyRSS2Gen.RSSItem(
-        title=Title, link=Link,
-        guid=PyRSS2Gen.Guid(Link)))
+# RSSItemList = []
+# for Title, Link in zip(titles, urls):
+#    RSSItemList.append(PyRSS2Gen.RSSItem(
+#        title=Title, link=Link,
+#        guid=PyRSS2Gen.Guid(Link)))
 
 if makeRSS:
-    rss = PyRSS2Gen.RSS2(
-        title=channelTitle,
-        link=channelLink,
-        description=channelDescription,
-        generator="BBC Three RSS generator",
-        docs="http://cyber.harvard.edu/rss/rss.html",
-        lastBuildDate=datetime.datetime.now(),
-        items=RSSItemList
+#    rss = PyRSS2Gen.RSS2(
+#        title=channelTitle,
+#        link=channelLink,
+#        description=channelDescription,
+#        generator="BBC Three RSS generator",
+#        docs="http://cyber.harvard.edu/rss/rss.html",
+#        lastBuildDate=datetime.datetime.now(),
+#        items=RSSItemList
+#    )
+
+    feed = RSS2Feed(
+    title=channelTitle,
+    link=channelLink,
+    description=channelDescription
     )
 
-rss.write_xml(open("/var/www/html/bbc-three-rss.xml", "w", encoding="utf-8"))  # encoding very important, breaks otherwise!
+    for Title, Link in zip(titles, urls):
+        feed.append_item(
+        title=Title,
+        link=Link)
+
+    x = feed.get_xml()
+    print(x)
+    file = open("/var/www/html/bbc-three-rss.xml", "w")
+    file.write(x)
+
+# rss.write_xml(open("/var/www/html/bbc-three-rss.xml", "w", encoding="utf-8"))  # encoding very important, breaks otherwise!
